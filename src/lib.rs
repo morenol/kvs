f93a792cffb37228ea56255c025c74ac4137f46a
Luis Moreno <luis.moreno@edunext.co>
2020-03-07 17:23:50 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+pub struct KvStore{
+
+}
+
+impl KvStore{
+   pub fn get(&self, _key: String) -> Option<String> {
+    unimplemented!("unimplemented");
+   }
+   pub fn new() -> KvStore {
+    unimplemented!("unimplemented");
+   }
+   pub fn set(&mut self, _key: String, _value: String) {
+    unimplemented!("unimplemented");
+   }
+
+   pub fn remove(&mut self, _key: String) {
+    unimplemented!("unimplemented");
+   }
+
+}
+
