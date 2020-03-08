3af3bc7959d3103eb42a9e4b301b90123d64403d
Luis Moreno <luis.moreno@edunext.co>
2020-03-07 22:57:12 -0400

    Run cargo fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,21 +1,18 @@
 #[macro_use]
 extern crate clap;
-use clap::{App,AppSettings};
+use clap::{App, AppSettings};
 use std::env;
-
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    
 
     let matches = App::from_yaml(yaml)
-                            .author(crate_authors!())
-                            .version(crate_version!())
-                            .about(env!("CARGO_PKG_DESCRIPTION"))
-                            .setting(AppSettings::DisableHelpSubcommand)
-                            .setting(AppSettings::SubcommandRequiredElseHelp)
-                            .get_matches();
-
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("get") {
         unimplemented!("set is unimplemented");
@@ -28,7 +25,4 @@ fn main() {
     if let Some(_matches) = matches.subcommand_matches("rm") {
         unimplemented!("set is unimplemented");
     }
-
-
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,69 @@
-
 use std::collections::HashMap;
 
 /// Key-Value store structure.
-pub struct KvStore{
-   /// A HashMap from the std lib is used to store the key,value elements.
-   m_hash: HashMap<String,String>,
+pub struct KvStore {
+    /// A HashMap from the std lib is used to store the key,value elements.
+    m_hash: HashMap<String, String>,
 }
 
 impl Default for KvStore {
     fn default() -> Self {
-       let m_hash = HashMap::new();
-       KvStore{
-           m_hash
-       }
-   }
-}       
-impl KvStore{
-   /// Get value of a given key in the KV store.
-   ///
-   ///```
-   ///use kvs::KvStore;
-   ///let store = KvStore::new();
-   ///let missing = store.get("missing_key".to_owned());
-   ///assert_eq!(missing, None);
-   ///```
-   pub fn get(&self, key: String) -> Option<String> {
-      let value = self.m_hash.get(&key);
-      match value {
-            Some(s) => Some(s.clone()),
-            _ => None
-        }
-   }
-
-   /// Initialize an empty KvStore
-   /// ```
-   /// use kvs::KvStore;
-   /// let store = KvStore::new();
-   /// ```
-   pub fn new() -> KvStore {
-       KvStore::default()
-   }
-
-   ///
-   /// Set value of a key in in the KV store
-   /// ```
-   /// use kvs::KvStore;
-   /// let mut store = KvStore::new();
-   /// store.set("key1".to_owned(), "value1".to_owned());
-   /// let value = store.get("key1".to_owned()).unwrap();
-   /// assert_eq!(value, "value1");
-   /// ```
-   pub fn set(&mut self, key: String, value: String) {
-      self.m_hash.insert(key, value);
-   }
-
-   /// Remove key-value from the KV store
-   /// ```
-   /// use kvs::KvStore;
-   /// let mut store = KvStore::new();
-   /// store.set("key1".to_owned(), "value1".to_owned());
-   /// let value = store.get("key1".to_owned()).unwrap();
-   /// assert_eq!(value, "value1");
-   /// store.remove("key1".to_owned());
-   /// let missing = store.get("missing_key".to_owned());
-   /// assert_eq!(missing, None);
-   /// ```
-
-   pub fn remove(&mut self, key: String) {
-       self.m_hash.remove(&key);
-   }
-
+        let m_hash = HashMap::new();
+        KvStore { m_hash }
+    }
 }
+impl KvStore {
+    /// Get value of a given key in the KV store.
+    ///
+    ///```
+    ///use kvs::KvStore;
+    ///let store = KvStore::new();
+    ///let missing = store.get("missing_key".to_owned());
+    ///assert_eq!(missing, None);
+    ///```
+    pub fn get(&self, key: String) -> Option<String> {
+        let value = self.m_hash.get(&key);
+        match value {
+            Some(s) => Some(s.clone()),
+            _ => None,
+        }
+    }
 
+    /// Initialize an empty KvStore
+    /// ```
+    /// use kvs::KvStore;
+    /// let store = KvStore::new();
+    /// ```
+    pub fn new() -> KvStore {
+        KvStore::default()
+    }
+
+    ///
+    /// Set value of a key in in the KV store
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// let value = store.get("key1".to_owned()).unwrap();
+    /// assert_eq!(value, "value1");
+    /// ```
+    pub fn set(&mut self, key: String, value: String) {
+        self.m_hash.insert(key, value);
+    }
+
+    /// Remove key-value from the KV store
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// let value = store.get("key1".to_owned()).unwrap();
+    /// assert_eq!(value, "value1");
+    /// store.remove("key1".to_owned());
+    /// let missing = store.get("missing_key".to_owned());
+    /// assert_eq!(missing, None);
+    /// ```
+
+    pub fn remove(&mut self, key: String) {
+        self.m_hash.remove(&key);
+    }
+}
