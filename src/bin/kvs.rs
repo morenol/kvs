c71ac7dbe0f656f0ed68f24e3b0f1cf1bd9731d3
Luis Moreno <morenoluisma@gmail.com>
2020-04-05 23:55:33 -0400

    Store log pointers in the index.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate clap;
 use clap::{App, AppSettings};
+use kvs::{KvStore, Result};
 use std::env;
-use kvs::{KvStore,Result};
 
 fn main() -> Result<()> {
     let yaml = load_yaml!("cli.yml");
@@ -21,7 +21,7 @@ fn main() -> Result<()> {
         let res = storage.get(key.to_string())?;
         match res {
             Some(value) => println!("{}", value),
-            None => println!("Key not found")
+            None => println!("Key not found"),
         };
     }
 
@@ -29,17 +29,20 @@ fn main() -> Result<()> {
         let mut storage = KvStore::open(".")?;
         let key = matches.value_of("KEY").unwrap();
         let value = matches.value_of("VALUE").unwrap();
-        storage.set(key.to_string(), value.to_string());
-     }
+        storage.set(key.to_string(), value.to_string())?;
+    }
 
     if let Some(matches) = matches.subcommand_matches("rm") {
         let mut storage = KvStore::open(".")?;
         let key = matches.value_of("KEY").unwrap();
         let res = storage.remove(key.to_string());
         return match res {
-                Ok(_) => Ok(()),
-                Err(err) => {println!("{}", err.to_string() );  Err(err)}
-        }
+            Ok(_) => Ok(()),
+            Err(err) => {
+                println!("{}", err.to_string());
+                Err(err)
+            }
+        };
     }
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,51 @@
-use std::collections::HashMap;
-use std::path::PathBuf;
-use std::result;
-use std::fs::{File, OpenOptions};
 use failure::Error;
 use serde::{Deserialize, Serialize};
-use std::io::{BufReader,BufRead, BufWriter, Seek, SeekFrom, Write};
-#[macro_use] extern crate failure;
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write};
+use std::path::PathBuf;
+use std::result;
+#[macro_use]
+extern crate failure;
 
 pub type Result<T> = result::Result<T, Error>;
 
 /// Key-Value store structure.
 pub struct KvStore {
     /// A HashMap from the std lib is used to store the key,value elements.
-    m_hash: HashMap<String, String>,
+    m_hash: HashMap<String, u64>,
 
     /// File used to record the set and rm operations.
-    file: File
+    file: File,
 }
 
 impl KvStore {
+    fn init(&mut self) -> Result<()> {
+        let mut reader = BufReader::new(&self.file);
 
+        loop {
+            let position = reader.seek(SeekFrom::Current(0))?;
 
-    fn init(& mut self) -> Result<()> {
-        let reader = BufReader::new(&self.file);
+            let mut line = String::new();
+            let lize_size = reader.read_line(&mut line)?;
 
-    for line in reader.lines(){
-        let command : Command = serde_json::from_str(&line?[..])?;
-        match command {
-            Command::Rm(key) => self.m_hash.remove(&key),
-            Command::Set(key, value) => self.m_hash.insert(key, value)
-        };
+            if lize_size > 0 {
+                let command: Command = serde_json::from_str(&line[..])?;
+                match command {
+                    Command::Rm(key) => self.m_hash.remove(&key),
+                    Command::Set(key, _value) => self.m_hash.insert(key, position),
+                };
+            } else {
+                break;
+            }
+        }
+        Ok(())
     }
-    Ok(())
-    }
 
-    fn exists(& self, key: String) -> bool {
+    fn exists(&self, key: String) -> bool {
         match self.m_hash.get(&key) {
             Some(_s) => true,
-            None => false
+            None => false,
         }
     }
 
@@ -50,22 +58,34 @@ impl KvStore {
     ///assert_eq!(missing, None);
     ///```
     pub fn get(&self, key: String) -> Result<Option<String>> {
-        let value = self.m_hash.get(&key);
-        match value {
-            Some(s) => Ok(Some(s.clone())),
+        let stored = self.m_hash.get(&key);
+        match stored {
+            Some(&position) => {
+                let mut reader = BufReader::new(&self.file);
+                let mut line = String::new();
+                reader.seek(SeekFrom::Start(position))?;
+                reader.read_line(&mut line)?;
+                let command: Command = serde_json::from_str(&line[..])?;
+                let value = match command {
+                    Command::Set(_key, value) => value,
+                    _ => panic!("Invalid state"),
+                };
+
+                Ok(Some(value))
+            }
             _ => Ok(None),
         }
     }
 
-    fn log(&mut self, command: &Command) -> Result<()> {
+    fn log(&mut self, command: &Command) -> Result<u64> {
         let mut wr = BufWriter::new(&self.file);
-        wr.seek(SeekFrom::End(0))?;
+        let position = wr.seek(SeekFrom::End(0))?;
         serde_json::to_writer(wr, &command).unwrap();
 
         let mut wr = BufWriter::new(&self.file);
         wr.seek(SeekFrom::End(0))?;
         wr.write_all(b"\n")?;
-        Ok(())
+        Ok(position)
     }
 
     pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
@@ -74,23 +94,19 @@ impl KvStore {
         path.push("kvs.log");
 
         let file = OpenOptions::new()
-                        .read(true)
-                        .write(true)
-                        .create(true)
-                        .open(path)?;
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)?;
 
         let m_hash = HashMap::new();
 
-        let mut storage = KvStore{
-            m_hash,
-            file,
-        };
+        let mut storage = KvStore { m_hash, file };
 
         storage.init()?;
 
         Ok(storage)
     }
-
 
     ///
     /// Set value of a key in in the KV store
@@ -103,8 +119,8 @@ impl KvStore {
     /// ```
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let command = Command::Set(key.clone(), value.clone());
-        self.log(&command)?;
-        self.m_hash.insert(key, value);
+        let position = self.log(&command)?;
+        self.m_hash.insert(key, position);
         Ok(())
     }
 
@@ -121,13 +137,12 @@ impl KvStore {
     /// ```
     pub fn remove(&mut self, key: String) -> Result<()> {
         let command = Command::Rm(key.clone());
-        match self.exists(key.clone()){
-            false => Err(format_err!("Key not found")),
-            true => {
-                self.log(&command)?;
-                self.m_hash.remove(&key);
-                Ok(())
-            }
+        if self.exists(key.clone()) {
+            self.log(&command)?;
+            self.m_hash.remove(&key);
+            Ok(())
+        } else {
+            Err(format_err!("Key not found"))
         }
     }
 }
@@ -135,5 +150,5 @@ impl KvStore {
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
     Rm(String),
-    Set(String, String)
+    Set(String, String),
 }
